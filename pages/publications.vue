21ba011d356f704ebb23d69e2e00a52fe93948cc
olivierglrx <olivier.glrx@gmail.com>
2024-02-27 12:21:00 +0100

    flex wrap

--- pages/publications.vue
+++ pages/publications.vue
@@ -2,7 +2,7 @@
   
   <h1 class="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Publications</h1>
 
-  <div class="container text-center justify-around	 ">
+  <div class="container text-center justify-around	flex-wrap">
     <div  v-for="publi in publications" class="border-slate-200 border m-px 	">
       <PersoCard1
         :title="publi.title.rendered"
